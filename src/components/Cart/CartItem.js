69ea130941914bc805578f2337a5da40d0f7a743
Admin <hiuteddy204@gmail.com>
2024-11-12 21:53:13 +0700

    hieu da sua lai gio hang lay userid luon dp can truyen

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,61 +1,44 @@
 import React from 'react';
 import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchtProductById, fetchtProductById1 } from '../../redux/actions/actionProduct';
-import handleProductPress from '../AllProduct/handleProductPress';
 
 const CartItem = ({ item, onIncrease, onDecrease, onRemove, navigation }) => {
 
-  const dispatch = useDispatch();
-  const product = useSelector(state => state.products.productById);
-
-  const handleFetchProduct = () => {
-    dispatch(fetchtProductById1(item.product_id._id))
-      .unwrap() // Nếu bạn dùng Redux Toolkit, `unwrap()` giúp lấy kết quả từ Promise của thunk
-      .then(itemProduct => {
-        navigation.navigate('ProductDetailScreen', { product: itemProduct });
-      })
-      .catch(error => {
-        console.error('Error fetching product:', error);
-      });
+  // Hàm điều hướng tới màn hình chi tiết sản phẩm
+  const handleNavigateToProductDetail = () => {
+    if (item.product_id) {
+      // Điều hướng tới ProductDetailScreen với sản phẩm đã có trong giỏ hàng
+      navigation.navigate('ProductDetailScreen', { product: item.product_id });
+    } else {
+      console.error('Product information is not available');
+    }
   };
-  
- 
-  
+
   return (
-    <TouchableOpacity onPress={handleFetchProduct}>
+    <TouchableOpacity onPress={handleNavigateToProductDetail}>
       <View style={styles.cartItem}>
-      <Image source={{uri:item.image_variant}} style={styles.itemImage} />
-      <View style={styles.itemInfo}>
-        <Text style={styles.itemName}>{item.name} - ({item.color})</Text>
-        <Text style={styles.itemPrice}>{item.price.toLocaleString()} VND</Text>
-        <Text style={styles.itemSize}>Size - {item.size}</Text>
-        <View style={styles.quantityContainer}>
-          <TouchableOpacity onPress={() => onDecrease(item._id)} style={styles.quantityButton}>
-            <Text style={styles.quantityText}>-</Text>
-          </TouchableOpacity>
-          <Text style={styles.quantityValue}>{item.quantity}</Text>
-          <TouchableOpacity onPress={() => onIncrease(item._id)} style={styles.quantityButton}>
-            <Text style={styles.quantityText}>+</Text>
+        <Image source={{ uri: item.image_variant }} style={styles.itemImage} />
+        <View style={styles.itemInfo}>
+          <Text style={styles.itemName}>{item.product_id?.name || 'Sản phẩm không xác định'} - ({item.color})</Text>
+          <Text style={styles.itemPrice}>{item.price.toLocaleString()} VND</Text>
+          <Text style={styles.itemSize}>Size - {item.size}</Text>
+          <View style={styles.quantityContainer}>
+            <TouchableOpacity onPress={() => onDecrease(item._id)} style={styles.quantityButton}>
+              <Text style={styles.quantityText}>-</Text>
+            </TouchableOpacity>
+            <Text style={styles.quantityValue}>{item.quantity}</Text>
+            <TouchableOpacity onPress={() => onIncrease(item._id)} style={styles.quantityButton}>
+              <Text style={styles.quantityText}>+</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={styles.buttonsContainer}>
+          <TouchableOpacity onPress={onRemove}>
+            <Image source={require('../../assets/images/icon_delete.png')} />
           </TouchableOpacity>
         </View>
+        <View style={styles.separator}></View>
       </View>
-      <View style={styles.buttonsContainer}>
-        {/* Add to Cart Button */}
-    
-
-        <TouchableOpacity onPress={onRemove}>
-        <Image source={require('../../assets/images/icon_delete.png')}/>
-        </TouchableOpacity>
-
-        {/* Remove from Favorites Button */}
-      
-      </View>
-      <View style={styles.separator}></View>
-
-    </View>
     </TouchableOpacity>
-    
   );
 };
 
@@ -64,7 +47,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     padding: 10,
     marginVertical: 5,
-    borderRadius: 10,  
+    borderRadius: 10,
   },
   itemImage: {
     width: 100,
@@ -76,17 +59,16 @@ const styles = StyleSheet.create({
     marginLeft: 16,
   },
   itemName: {
-    fontFamily:'Nunito Sans',
+    fontFamily: 'Nunito Sans',
     fontSize: 14,
     color: '#999999',
   },
   itemPrice: {
-    fontFamily:'Nunito Sans',
+    fontFamily: 'Nunito Sans',
     fontSize: 16,
     color: '#242424',
     fontWeight: 'bold',
     marginTop: 4,
-
   },
   itemSize: {
     fontSize: 13,
@@ -99,10 +81,9 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   buttonsContainer: {
-    justifyContent:'flex-start',
-    margin:5
+    justifyContent: 'flex-start',
+    margin: 5,
   },
-
   quantityButton: {
     width: 30,
     height: 30,
@@ -116,23 +97,20 @@ const styles = StyleSheet.create({
   },
   quantityValue: {
     marginHorizontal: 10,
-    fontFamily:'Nunito Sans',
+    fontFamily: 'Nunito Sans',
     fontSize: 16,
     color: '#242424',
     fontWeight: 'bold',
-
   },
   separator: {
     position: 'absolute',
     bottom: 0,
     left: 0,
     right: 0,
-    marginHorizontal:20,
-    height: 1,  // Adjust the thickness of the line
-    backgroundColor: '#E0E0E0',  // Light grey color to resemble the white line in the image
+    marginHorizontal: 20,
+    height: 1,
+    backgroundColor: '#E0E0E0',
   },
-  
- 
 });
 
 export default CartItem;
--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -1,87 +1,74 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { addTocCart, deleteCartItem, fetchCart, updatequantity } from '../actions/actionCart';
 
-
 const initialState = {
-  cart: [],
-  cartLength: null,
-  isLoading: false,
-  isLoadingQuantity:false,
-  error: null,
+    cart: [],
+    cartLength: null,
+    isLoading: false,
+    isLoadingQuantity: false,
+    error: null,
 };
 
 const cartSlice = createSlice({
-  name: 'cart',
-  initialState,
-  reducers: {},
-
-  extraReducers: (builder) => {
-    builder
-      // Xử lý khi lấy cartItem
-      .addCase(fetchCart.pending, (state) => {
-        console.log('Fetch Cart Pending'); // Debug pending
-        state.isLoading = true;
-      })
-      .addCase(fetchCart.fulfilled, (state, action) => {
-        console.log('Fetch Cart Fulfilled Payload:', action.payload); // Debug payload
-        state.isLoading = false;
-        state.cart = action.payload;
-        state.cartLength = state.cart.length;
-      })
-      .addCase(fetchCart.rejected, (state, action) => {
-        console.error('Fetch Cart Rejected:', action.payload); // Debug rejected
-        state.isLoading = false;
-        state.error = action.payload; // sử dụng thông tin từ `thunkAPI.rejectWithValue`
-      })
-
-      .addCase(addTocCart.pending, (state) => {
-        state.isLoading = true;
-        state.error = null;
-      })
-      .addCase(addTocCart.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.cart.push(action.payload); // Thêm địa chỉ mới vào danh sách
-        state.error = null;
-      })
-      .addCase(addTocCart.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.error.message;
-      })
-
-      .addCase(updatequantity.pending, (state) => {
-        state.isLoadingQuantity = true;
-        state.error = null;
-      })
-      .addCase(updatequantity.fulfilled, (state, action) => {
-        state.isLoadingQuantity = false;
-        // Cập nhật sản phẩm với số lượng mới
-        const updatedCart = state.cart.map((item) =>
-          item._id === action.payload._id ? { ...item, quantity: action.payload.quantity } : item
-        );
-        state.cart = updatedCart;
-        state.error = null;
-      })
-      .addCase(updatequantity.rejected, (state, action) => {
-        state.isLoadingQuantity = false;
-        state.error = action.error.message;
-      })
-
-      .addCase(deleteCartItem.pending, (state) => {
-        state.error=null
-      })
-      .addCase(deleteCartItem.fulfilled, (state, action) => {
-        console.log('Deleted Item _id:', action.payload); // Debug _id được trả về từ action.payload
-        state.cart = state.cart.filter(cart => cart._id !== action.payload); // Thay đổi trực tiếp state.cart
-        state.cartLength = state.cart.length; 
-        state.error = null;
-      })
-      
-      
-      .addCase(deleteCartItem.rejected, (state, action) => {
-        state.isLoadingQuantity = false;
-        state.error = action.error.message;
-      })
-  }
+    name: 'cart',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchCart.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(fetchCart.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.cart = action.payload;
+                state.cartLength = state.cart.length;
+            })
+            .addCase(fetchCart.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+            })
+            .addCase(addTocCart.pending, (state) => {
+                state.isLoading = true;
+                state.error = null;
+            })
+            .addCase(addTocCart.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.cart.push(action.payload);
+                state.error = null;
+            })
+            .addCase(addTocCart.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.error.message;
+            })
+            .addCase(updatequantity.pending, (state) => {
+                state.isLoadingQuantity = true;
+                state.error = null;
+            })
+            .addCase(updatequantity.fulfilled, (state, action) => {
+                state.isLoadingQuantity = false;
+                const updatedCart = state.cart.map((item) =>
+                    item._id === action.payload._id ? { ...item, quantity: action.payload.quantity } : item
+                );
+                state.cart = updatedCart;
+                state.error = null;
+            })
+            .addCase(updatequantity.rejected, (state, action) => {
+                state.isLoadingQuantity = false;
+                state.error = action.error.message;
+            })
+            .addCase(deleteCartItem.pending, (state) => {
+                state.error = null;
+            })
+            .addCase(deleteCartItem.fulfilled, (state, action) => {
+                state.cart = state.cart.filter(cart => cart._id !== action.payload);
+                state.cartLength = state.cart.length;
+                state.error = null;
+            })
+            .addCase(deleteCartItem.rejected, (state, action) => {
+                state.isLoadingQuantity = false;
+                state.error = action.error.message;
+            });
+    }
 });
 
 export default cartSlice.reducer;
